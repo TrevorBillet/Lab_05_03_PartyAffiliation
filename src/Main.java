7abaa4fb4a880beeb49d8eb3e194f978fe731198
Trevor Billet <98602126+TrevorBillet@users.noreply.github.com>
2022-02-11 11:02:04 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String partyAffil;
+        partyAffil = "O";
+     if (partyAffil == "D")
+        System.out.println("You got a Democratic Donkey!");
+     else if (partyAffil == "R")
+            System.out.println("You got a Republican Elephant!");
+     else if (partyAffil == "I")
+            System.out.println("You got an independent man!");
+     else
+         System.out.println("Incorrect party affliliation, try again!");
+
+    }
+}
